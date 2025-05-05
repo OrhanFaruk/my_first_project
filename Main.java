a9423cb4c31498cdcbd6c13c5a9cb510fd988dc2
Orhan Faruk Demir <orhanfarukdemir@unknown0ef1ab6f94f5.home>
2025-05-05 18:36:35 +0300

    Ilk java dosyasini ekliyorum.

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+// This is a simple Java program that prints "Hello, World!" to the console.
+// Purpose of this is to
+// understand how to push code to GitHub.
